bfb8c7046a05e2dc5c7ed4eb7405f3aee58edf1e
Matt Sell <mwsell@gmail.com>
2014-10-29 12:41:17 -0500

    introduce call back hell...to refactor later

--- server/controllers/players.js
+++ server/controllers/players.js
@@ -17,30 +17,57 @@ exports.reloadPlayerData = function(req, res){
     });
 
     response.on('end', function(){
-      console.log(byeWeekData);
-    });
-  }
+      console.log('got bye week data');
+      // get players
+      var playerOptions = {
+        host: 'myfantasyleague.com',
+        port: 80,
+        path: '/2014/export/export?TYPE=players&JSON=1',
+        method: 'GET'
+      };
 
-  var playerOptions = {
-    host: 'myfantasyleague.com',
-    port: 80,
-    path: '/2014/export/export?TYPE=players&JSON=1',
-    method: 'GET'
+      var playerData = '';
+      var playersCallback = function(playerResponse){
+        playerResponse.on('data', function(chunk){
+          playerData += chunk;
+        });
+
+        playerResponse.on('end', function(){
+          console.log('got players');
+          var players = JSON.parse(playerData).players.player;
+          console.log('MFL returned ' + players.length + ' players');
+          players.map(function(player){
+            // get details.
+            // map details and bye week to player
+            var playerDetailOptions = {
+              host: 'myfantasyleague.com',
+              port: 80,
+              path: '/2014/export/export/?TYPE=players&JSON=1&PLAYERS=' + player.id + '&DETAILS=1'
+            };
+            var playerDetails = '';
+            var detailsCallback = function(detailResponse){
+              detailResponse.on('data', function(chunk){
+                playerDetails += chunk;
+              });
+
+              detailResponse.on('end', function(){
+                console.log('got details');
+                playerDetails = JSON.parse(playerDetails).players.player;
+                //Player.create()
+              });
+            };
+            // need to await the previous request or face epic fail
+            http.request(playerDetailOptions, detailsCallback).end();
+          });
+        });
+      };
+
+      http.request(playerOptions, playersCallback).end();
+    });
   };
 
-  var playerData = '';
-  var playersCallback = function(response){
-    response.on('data', function(chunk){
-      playerData += chunk;
-    });
-
-    response.on('end', function(){
-      console.log(playerData);
-    });
-  }
-
   http.request(byeOptions, byeWeeksCallback).end();
-  http.request(playerOptions, playersCallback).end();
+
   res.sendStatus(202);
 
 };
