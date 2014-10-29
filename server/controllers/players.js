0cac7e610e1fadd28c447bc9e83a77e04831c5f3
Matt Sell <mwsell@gmail.com>
2014-10-28 21:54:42 -0500

    reach out to myfantasyleague for player data

--- server/controllers/players.js
+++ server/controllers/players.js
@@ -2,26 +2,45 @@ var Player = require('mongoose').model('Player'),
   http = require('http');
 
 exports.reloadPlayerData = function(req, res){
-  var options = {
+  var byeOptions = {
     host: 'www.fantasyfootballnerd.com',
     port: 80,
     path: '/service/byes/json/k3bppf468tzs',
     method: 'GET'
   };
 
-  var callback = function(response){
-    var str = '';
+  var byeWeekData = '';
+
+  var byeWeeksCallback = function(response){
     response.on('data', function(chunk){
-      str += chunk;
+      byeWeekData += chunk;
     });
 
     response.on('end', function(){
-      console.log(str);
+      console.log(byeWeekData);
     });
   }
 
-  http.request(options, callback).end();
+  var playerOptions = {
+    host: 'myfantasyleague.com',
+    port: 80,
+    path: '/2014/export/export?TYPE=players&JSON=1',
+    method: 'GET'
+  };
 
+  var playerData = '';
+  var playersCallback = function(response){
+    response.on('data', function(chunk){
+      playerData += chunk;
+    });
+
+    response.on('end', function(){
+      console.log(playerData);
+    });
+  }
+
+  http.request(byeOptions, byeWeeksCallback).end();
+  http.request(playerOptions, playersCallback).end();
   res.sendStatus(202);
 
 };
