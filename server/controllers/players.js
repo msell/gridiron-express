578f1a415f0cd2c9338c4a1b76d45f80967b2a34
Matt Sell <mwsell@gmail.com>
2014-10-28 21:25:37 -0500

    make api call out to bye weeks

--- server/controllers/players.js
+++ server/controllers/players.js
@@ -1,7 +1,27 @@
-var Player = require('mongoose').model('Player');
+var Player = require('mongoose').model('Player'),
+  http = require('http');
 
 exports.reloadPlayerData = function(req, res){
-  console.log('do stuff here..');
-  res.send(202);
+  var options = {
+    host: 'www.fantasyfootballnerd.com',
+    port: 80,
+    path: '/service/byes/json/k3bppf468tzs',
+    method: 'GET'
+  };
+
+  var callback = function(response){
+    var str = '';
+    response.on('data', function(chunk){
+      str += chunk;
+    });
+
+    response.on('end', function(){
+      console.log(str);
+    });
+  }
+
+  http.request(options, callback).end();
+
+  res.sendStatus(202);
 
 };
