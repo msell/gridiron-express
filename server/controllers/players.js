0cc340a3a6a0bec49adb64ab74d97f252b6256da
Matt Sell <mwsell@gmail.com>
2014-10-29 22:10:32 -0500

    extract some methods

--- server/controllers/players.js
+++ server/controllers/players.js
@@ -1,73 +1,88 @@
 var Player = require('mongoose').model('Player'),
   http = require('http');
 
-exports.reloadPlayerData = function(req, res){
-  var byeOptions = {
+exports.reloadPlayerData = function(req, res) {
+  getByeWeekData();
+  var players = getPlayerData();
+  
+
+  res.sendStatus(202);
+};
+
+
+function getByeWeekData() {
+  var byeWeekData = '';
+
+  var options = {
     host: 'www.fantasyfootballnerd.com',
     port: 80,
     path: '/service/byes/json/k3bppf468tzs',
     method: 'GET'
   };
-
-  var byeWeekData = '';
-
-  var byeWeeksCallback = function(response){
-    response.on('data', function(chunk){
+  var callback = function (response) {
+    response.on('data', function (chunk) {
       byeWeekData += chunk;
     });
 
-    response.on('end', function(){
+    response.on('end', function () {
       console.log('got bye week data');
-      // get players
-      var playerOptions = {
-        host: 'myfantasyleague.com',
-        port: 80,
-        path: '/2014/export/export?TYPE=players&JSON=1',
-        method: 'GET'
-      };
-
-      var playerData = '';
-      var playersCallback = function(playerResponse){
-        playerResponse.on('data', function(chunk){
-          playerData += chunk;
-        });
-
-        playerResponse.on('end', function(){
-          console.log('got players');
-          var players = JSON.parse(playerData).players.player;
-          console.log('MFL returned ' + players.length + ' players');
-          players.map(function(player){
-            // get details.
-            // map details and bye week to player
-            var playerDetailOptions = {
-              host: 'myfantasyleague.com',
-              port: 80,
-              path: '/2014/export/export/?TYPE=players&JSON=1&PLAYERS=' + player.id + '&DETAILS=1'
-            };
-            var playerDetails = '';
-            var detailsCallback = function(detailResponse){
-              detailResponse.on('data', function(chunk){
-                playerDetails += chunk;
-              });
-
-              detailResponse.on('end', function(){
-                console.log('got details');
-                playerDetails = JSON.parse(playerDetails).players.player;
-                //Player.create()
-              });
-            };
-            // need to await the previous request or face epic fail
-            http.request(playerDetailOptions, detailsCallback).end();
-          });
-        });
-      };
-
-      http.request(playerOptions, playersCallback).end();
     });
   };
 
-  http.request(byeOptions, byeWeeksCallback).end();
+    http.request(options, callback).end();
 
-  res.sendStatus(202);
+    return byeWeekData;
+}
+  function getPlayerData(){
+    var playerData = '';
+    var players = [];
+    var options = {
+      host: 'myfantasyleague.com',
+      port: 80,
+      path: '/2014/export/export?TYPE=players&JSON=1',
+      method: 'GET'
+    };
 
-};
+    var callback = function(response) {
+      response.on('data', function (chunk) {
+        playerData += chunk;
+      });
+
+      response.on('end', function () {
+        console.log('got players');
+        var players = JSON.parse(playerData).players.player;
+        console.log('MFL returned ' + players.length + ' players');
+      });
+    };
+
+      http.request(options, callback).end();
+
+    return players;
+  }
+
+  function getPlayerDetails(playerId) {
+    var playerDetails = '';
+
+    var options = {
+      host: 'myfantasyleague.com',
+      port: 80,
+      path: '/2014/export/export/?TYPE=players&JSON=1&PLAYERS=' + player.id + '&DETAILS=1'
+    };
+
+    var callback = function(detailResponse){
+      detailResponse.on('data', function(chunk){
+        playerDetails += chunk;
+      });
+
+      detailResponse.on('end', function(){
+        console.log('got details');
+        playerDetails = JSON.parse(playerDetails).players.player;
+        //Player.create()
+      });
+    };
+
+      http.request(options, callback).end();
+
+      return playerDetails;
+  }
+
